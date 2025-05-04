25ea100bd52a9afc7443c37cf14cd3673bf3aaef
Keisuke <keisuke.sugawara@spacedata.co.jp>
2025-05-04 20:55:25 +0900

    [add][update]add rosbag convert script and plot script

--- rosbag_record.sh
+++ rosbag_record.sh
@@ -1,4 +1,7 @@
-output_dir_path="./rosbag2_out/rosbag2_$(date +%Y-%m-%d_%H-%M-%S)/"
+
+output_base_dir_path="./rosbag2_out"
+mkdir -p output_base_dir_path
+output_dir_path="${output_base_dir_path}/rosbag2_$(date +%Y-%m-%d_%H-%M-%S)/"
 
 ros2 bag record -o ${output_dir_path} \
 /sarj_angle \
@@ -6,4 +9,7 @@ ros2 bag record -o ${output_dir_path} \
 /soc \
 /ss_attitude \
 /ss_in_sunlight \
-/sun_direction_ssbf
+/sun_direction_ssbf \
+/ss_position_eci \
+/ss_velocity_eci \
+/ss_acceleration_eci
